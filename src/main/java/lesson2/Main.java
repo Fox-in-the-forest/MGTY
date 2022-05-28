ef4a5ff392858222e9bb79766f3420f030ebb5fb
Fox-in-the-forest <gologrin69@mail.ru>
2022-05-28 20:50:25 +0300

    lesson2 test

---
+++ src/main/java/lesson2/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson2;
+
+/**
+ *
+ * @author sugar
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
