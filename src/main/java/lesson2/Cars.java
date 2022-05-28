75bed5f60452cbbb8f1fe235e06b2cc0c396b058
Fox-in-the-forest <gologrin69@mail.ru>
2022-05-28 23:20:05 +0300

    lesson2.Cars

---
+++ src/main/java/lesson2/Cars.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson2;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author sugar
+ */
+public class Cars {
+    private static final String SFM_UNDEFINED = "UNDEFINED";
+    
+    private final int mHorsePower;
+    private final int mVolume;
+    private final String mRegNum;
+
+    
+    public Cars(final int pHorsePower, final int pVolume, final String pRegNum)
+    {
+        super();
+        this.mHorsePower = pHorsePower;
+        this.mVolume = pVolume;
+        this.mRegNum = pRegNum;
+        
+    }
+
+    public int getmHorsePower() {
+        return mHorsePower;
+    }
+    public int getmVolume() {
+        return mVolume;
+    }
+    public String getmRegNum() {
+        return Optional.ofNullable(mRegNum).orElse(SFM_UNDEFINED);
+    }
+    
+    
+}
