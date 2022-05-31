1f238d14b8b4df768d691bfb7d669956ac9ce936
Fox-in-the-forest <gologrin69@mail.ru>
2022-05-31 12:41:48 +0300

    lesson2. add new classes

---
+++ src/main/java/lesson2/Bus.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson2;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author sugar
+ */
+public class Bus extends Cars{
+
+    
+    private final String mProperty; 
+
+    public String getProperty() {
+        return Optional.ofNullable(mProperty).orElse(Constants.SFM_UNDEFINED);
+    }
+    
+    
+    public Bus(int pHorsePower, int pVolume, String pRegNum, String pProperty) {
+        super(pHorsePower, pVolume, pRegNum);
+        mProperty = pProperty;
+    }
+
+
+    
+    
+}
---
+++ src/main/java/lesson2/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson2;
+
+/**
+ *
+ * @author sugar
+ */
+public class Constants {
+    public static final String SFM_UNDEFINED = "UNDEFINED";
+    
+}
