ebc5482af9760dfeba15849f795a64409f486e46
Fox-in-the-forest <gologrin69@mail.ru>
2022-05-31 12:44:33 +0300

    lesson2. add new class psvm

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -10,6 +10,30 @@ package lesson2;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        
+        Cars lBus = new Bus(100, 100, "43678", "PROPERTY");
+        Cars lTrucks = new Trucks(100, 100, "43678", 189);
+        Bus lBus1 = new Bus(100, 100, "43678", "PROPERTY");
+        Trucks lTrucks1 = new Trucks(100, 100, "43678", 189);
+        printCharacteristics(lBus1);
+        printCharacteristics((Bus) lBus); // приведение типов!
     }
+    
+    
+    //1 вариант
+    public static void printCharacteristics(Bus pBus)
+    {
+        
+    }
+    public static void printCharacteristics(Trucks pBus)
+    {
+        
+    }
+        
+    //2 вариант
+    public static void printCharacteristicsCars(Cars pBus)
+    {
+        
+    }
+        
 }
