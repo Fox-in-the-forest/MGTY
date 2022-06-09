96855857bee57434c3931bf1da841252ecffbd5e
Fox-in-the-forest <gologrin69@mail.ru>
2022-06-09 15:32:50 +0300

    lesson2.interface

---
+++ src/main/java/lesson2/ICars.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lesson2;
+
+/**
+ *
+ * @author sugar
+ */
+public interface ICars {
+    
+    int getmHorsePower();
+    int getmVolume();
+    String getmRegNum();
+    void printAdditionalProperty();
+}
