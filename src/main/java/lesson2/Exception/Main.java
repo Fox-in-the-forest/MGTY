5081f730dab6c9e12c29f4c60623c63ccd7ad829
Fox-in-the-forest <gologrin69@mail.ru>
2022-06-13 23:29:26 +0300

    lesson2.Exception: try/catch

--- src/main/java/lesson2/Exception/Main.java
+++ src/main/java/lesson2/Exception/Main.java
@@ -9,7 +9,34 @@ package lesson2.Exception;
  * @author sugar
  */
 public class Main {
-    public static void main(String[] args) {
-        //Throwable ->   CheckException<- Ecxeption ->RuntimeException
+    public static void main(String[] args) throws Exception {
+//Throwable ->  CheckException<- Ecxeption ->RuntimeException -> Unchekexception
+    make();
+    try
+    {
+        make();
     }
+    catch(MainException pE)
+    {
+        System.out.println(pE.getMessage());
+        pE.printStackTrace();
+    }
+        System.out.println("Program finished");
+    }
+    
+    public static void make() throws MainException
+    {
+         make1();
+    }
+        public static void make1() throws MainException
+    {
+         int i =1;
+         //Так делать нельзя
+         if(i<10)
+         {
+             throw new MainException("i>10");
+         }
+    }
+        
 }
+ 
