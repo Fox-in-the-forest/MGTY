7946a875368234a9be5c2a30a98b83008c12c5aa
Fox-in-the-forest <gologrin69@mail.ru>
2022-09-16 14:23:12 +0300

    commit5 - initialize an array + method

--- src/lesson03/Collections/ForCollections.java
+++ src/lesson03/Collections/ForCollections.java
@@ -1,11 +1,26 @@
 package lesson03.Collections;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ForCollections {
 	
 	public static void main(String[] args) 
 	{
-		
+		String[] pStringArray1 = initArray(2);
+	
 	}
 	
+	public static String[] initArray(final int pSize) {
+		
+		String[] pStringArray1 = new String[pSize];
+		for(int i = 0; i<pStringArray1.length; i++) {
+			pStringArray1[i] = String.valueOf("AAFFF");
+			System.out.println(pStringArray1[i]);
+		}
+		
+		return pStringArray1; 
+	}
+ 	
 }
 
