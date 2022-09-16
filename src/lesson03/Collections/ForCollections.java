3f4b58b9597449706624685210dcc192bb60a2d4
Fox-in-the-forest <gologrin69@mail.ru>
2022-09-16 20:26:10 +0300

    commit7 - iteration, examples

--- src/lesson03/Collections/ForCollections.java
+++ src/lesson03/Collections/ForCollections.java
@@ -16,17 +16,38 @@ public class ForCollections {
 		
 		String[] pStringArray1 = new String[pSize];
 		for(int i = 0; i<pStringArray1.length; i++) {
-			pStringArray1[i] = String.valueOf("AAFFF");
+			pStringArray1[i] = String.valueOf("AAFFF") + i;
 			
 		}
+		//while
+		int index1 = 0;
+		while(index1<pStringArray1.length) 
+		{
+			pStringArray1[index1]= pStringArray1[index1]+" add";
+			index1++;
+		}
+		//do while
+		int index2 = 0;
+		do
+		{
+			pStringArray1[index2]= pStringArray1[index2]+" addNew";
+			index2++;
+		} 
+		while(index2<pStringArray1.length);
 		
 		return pStringArray1; 
 	}
  	
 	public static void printArray(final String[] pStringArray1) 
 	{
+		int index = 0;
+		//forEach
 		for(String lString: pStringArray1) {
-			System.out.println("OUR VALUE: "+ lString);
+			System.out.println("VALUE: "+ lString);
+			if(index ==4) {
+				System.out.println("5:::VALUE: "+ lString);
+			}
+			index++;
 		}
 	}
 }
