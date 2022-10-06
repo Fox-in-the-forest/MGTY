2909c46d6a7dbf977c3b03b3c4d0e281cfe26ca1
Fox-in-the-forest <gologrin69@mail.ru>
2022-10-06 13:44:30 +0300

    commit10 - add; java 1.8+ / java 1.8-

---
+++ src/lesson03/Collections/ForArrays.java
@@ -0,0 +1,10 @@
+package lesson03.Collections;
+
+public class ForArrays {
+
+	public static void main(String[] args) {
+		System.out.println(String.class.getName());
+		System.out.println("args: "+ args.getClass().getName());
+	}
+
+}
