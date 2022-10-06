da3c00c39a8781ce241283c9c57ae55bb594943c
Fox-in-the-forest <gologrin69@mail.ru>
2022-10-06 21:39:23 +0300

    commit13

---
+++ src/lesson03/Collections/Worker.java
@@ -0,0 +1,11 @@
+package lesson03.Collections;
+
+public interface Worker {
+	void Work1(String p);
+	void Work2(String p);
+	void Work3(String p);
+	void Work4(String p);
+	void Work5(String p);
+	
+}
+ 
