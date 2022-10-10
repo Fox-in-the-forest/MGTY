1cb51167d8ebcc462ec84b2e1dea583555ca5bcb
Fox-in-the-forest <gologrin69@mail.ru>
2022-10-10 20:43:05 +0300

    commit15 -

---
+++ src/lesson03/Collections/CollectionExp.java
@@ -0,0 +1,30 @@
+package lesson03.Collections;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class CollectionExp {
+	
+	public static void main(String[] args) {
+		
+		Iterable<String> lIt1 = new ArrayList<>();
+		printScreen(lIt1);
+		
+		Collection<String> lIt2 = new ArrayList<>();
+		printScreen(lIt2);
+		
+		List<String> lIt3 = new ArrayList<>();
+		printScreen(lIt3);
+		
+		ArrayList<String> lIt4 = new ArrayList<>();
+		printScreen(lIt4);
+	}
+
+	
+	//ослабили тип с ArrayList до Iterable в printScreen методе
+	public static void printScreen(Iterable<String> pValue) 
+	{
+		pValue.forEach(pS -> System.out.println(pS));
+	}
+}
+ 
