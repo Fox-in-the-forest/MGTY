521e76e1738d4b3e206dc56f200e0fc75bd5f3dc
Fox-in-the-forest <gologrin69@mail.ru>
2022-10-18 13:27:28 +0300

    commit18 - ConcurrentModificationException

---
+++ src/lesson03/Collections/WorkWithCollection.java
@@ -0,0 +1,36 @@
+package lesson03.Collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class WorkWithCollection {
+
+	public WorkWithCollection() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) 
+	{
+		Collection<String> lStr = new ArrayList<>();
+		lStr.add("a");
+		lStr.add("b");
+		lStr.add("d");
+		lStr.add("s");
+		lStr.add("g");
+		lStr.add("h");
+		lStr.add("j");
+		lStr.add("t");
+		
+		for(String lString: lStr) 
+		{
+			if(Objects.equals(lString, "a")) 
+			{
+				lStr.remove(lString);
+			}
+			
+			lStr.add("ddff");
+		}
+	}
+
+}
