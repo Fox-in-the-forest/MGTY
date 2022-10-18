f33d3701d94c68be16545008c2912b9e5c3cc46f
Fox-in-the-forest <gologrin69@mail.ru>
2022-10-18 14:09:06 +0300

    commit20 - stream().

---
+++ src/lesson03/Collections/WorkWithCollection2.java
@@ -0,0 +1,37 @@
+package lesson03.Collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class WorkWithCollection2 {
+
+	public WorkWithCollection2() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) 
+	{
+		Collection<String> lStr = new ArrayList<>();
+		lStr.add("a");
+		lStr.add("b");
+		lStr.add("d");
+		lStr.add("s");
+		lStr.add(null);
+		lStr.add("g");
+		lStr.add("h"); 
+		lStr.add("j");
+		lStr.add("t");
+		lStr.add(null);
+		Collection<String> lStrRemove = new ArrayList<>();
+		// для работы с элементами Stream
+		
+		//Collection<String> lStr1 = lStrRemove.stream().filter(pS -> pS.equals("a")).collect(Collectors.toCollection(ArrayList::new));
+		//System.out.println(lStr.stream().filter(pS -> pS.equals("a")).collect(Collectors.toCollection(ArrayList::new)));
+		System.out.println(lStr.stream().filter(Objects::nonNull).filter(pS -> pS.equals("a")).collect(Collectors.toCollection(ArrayList::new)));
+
+	}
+
+}
