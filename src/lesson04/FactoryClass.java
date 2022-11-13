928075751bbcd68963d14c1d32d05920ade58d2b
Fox-in-the-forest <gologrin69@mail.ru>
2022-11-13 12:59:47 +0300

    commit5 - end

--- src/lesson04/FactoryClass.java
+++ src/lesson04/FactoryClass.java
@@ -44,7 +44,7 @@ public final class FactoryClass implements FactoryClassService {
 		
 		Builder()
 		{
-			
+			super();
 		}
 	
 
--- src/lesson04/Main4.java
+++ src/lesson04/Main4.java
@@ -6,9 +6,13 @@ public class Main4
 	{
 	FactoryClassService lfc = FactoryClass.createInstance();
 	FactoryClassService lfc1 = new FactoryClass();
-	
 	String lStr1 = FactoryClass.createInstance().Submit(); // фабричный метод
 	
+	FactoryClass.Builder lBuilder = FactoryClass.builder(); 
 	FactoryClassService lInstance = FactoryClass.builder().setmAge(0).setmName("Name").setmValue("Value").build();//метод строитель 
+	
+	FactoryClass1.builder().pStr1(lStr1).pStr2(lStr1);//метод строитель через lombok
+	FactoryClass1.buildInstance().mFiled1("one").mFiled2("two").build();
 	}
 }
+ 
